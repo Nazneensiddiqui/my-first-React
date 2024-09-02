86d7b08b5f723de20d5704d496450c0cb20b228c
naneen <nazneenbanosiddiqui@gmail.com>
2024-09-03 00:43:27 +0530

    second commit

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,7 +1,11 @@
+import image from '../public/des1.webp'
+
+
 const Hero = ()=>{
     return(
         <>
         <h1 style={{backgroundColor:'lightgreen'}}>this is hero section</h1>
+        <img   src={image} alt=""/>
         </>
     )
 }
--- src/footer.jsx
+++ src/footer.jsx
@@ -1,8 +1,12 @@
 const Footer = ()=>{
+    const num1 = 124;
+    const num2 = 20;
+
     return(
         <>
         <div>
             <h1 style={{color:'blue', backgroundColor:'yellow'}}>this is footer section</h1>
+            <h1 style = {{backgroundColor:"yellow",color:"red"}}> the sum of two number{num1+num2}</h1>
             </div>
             </>
     )
